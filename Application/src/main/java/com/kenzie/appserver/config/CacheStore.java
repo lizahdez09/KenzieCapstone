13d578a43959c90d30f887956c44acabfeded555
epentz88 <epentz88@gmail.com>
2023-06-07 19:25:24 -0500

    Lambda and Cache Setup

---
+++ Application/src/main/java/com/kenzie/appserver/config/CacheStore.java
@@ -0,0 +1,31 @@
+package com.kenzie.appserver.config;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.kenzie.appserver.service.model.Recipe;
+
+import java.util.concurrent.TimeUnit;
+
+public class CacheStore {
+
+    private Cache<String, Recipe> cache;
+
+    public CacheStore(int expiry, TimeUnit timeUnit) {
+        this.cache = CacheBuilder.newBuilder()
+                .expireAfterWrite(expiry, timeUnit)
+                .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+                .build();
+    }
+
+    public Recipe get(String key) {
+        return cache.getIfPresent(key);
+    }
+
+    public void evict(String key) {
+        cache.invalidate(key);
+    }
+
+    public void add(String key, Recipe value) {
+        cache.put(key, value);
+    }
+}
