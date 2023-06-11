65e08a11f46b54b080055c1f5a5b495eaac3c80e
bremartin95 <110068672+bremartin95@users.noreply.github.com>
2023-06-11 09:24:42 -0500

    made recipe table and recipeRecord, also devleloped some recipe service

--- Application/src/main/java/com/kenzie/appserver/repositories/model/RecipeRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/RecipeRecord.java
@@ -1,4 +1,44 @@
 package com.kenzie.appserver.repositories.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.Objects;
+
+@DynamoDBTable(tableName = "RecipeTable")
 public class RecipeRecord {
+    private String recipeId;
+    private String recipeName;
+
+
+    @DynamoDBAttribute(attributeName = "recipeId")
+    public String getRecipeId(){
+        return recipeId;
+    }
+    @DynamoDBHashKey(attributeName = "recipeName")
+    public String getRecipeName(){
+        return recipeName;
+    }
+
+    public void setRecipeId(String recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RecipeRecord that = (RecipeRecord) o;
+        return Objects.equals(recipeId, that.recipeId) && Objects.equals(recipeName, that.recipeName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(recipeId, recipeName);
+    }
 }
--- Application/src/main/java/com/kenzie/appserver/service/RecipeService.java
+++ Application/src/main/java/com/kenzie/appserver/service/RecipeService.java
@@ -4,20 +4,34 @@ import com.kenzie.appserver.repositories.RecipeRepository;
 import com.kenzie.appserver.service.model.Recipe;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RecipeService {
 
     private RecipeRepository recipeRepository;
+    private List<Recipe> recipes;
 
     public RecipeService(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
     }
 
-    public Recipe findById(String id) {
+    public Recipe getRecipeById(String id) {
+        for (Recipe recipe : recipes) {
+            if (recipe.equals(id)) { // needs to do .getById when its finished
+                return recipe;
+            }
+        }
         return null;
     }
 
     public Recipe addNewRecipe(String recipe) {
-        return null;
+
+        Recipe newRecipe = new Recipe(recipe);
+        recipes.add(newRecipe);
+        return newRecipe;
     }
+
+
+
 }
