ea27d9f52a0c29048d0088c450da7fbba4d950a4
stephensaulen <110068857+StephenSaulen@users.noreply.github.com>
2023-06-11 14:26:20 -0500

    Updated UserRecord

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/model/UserRecord.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/model/UserRecord.java
@@ -1,4 +1,45 @@
 package com.kenzie.capstone.service.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.Objects;
+
+@DynamoDBTable(tableName = "UserTable")
 public class UserRecord {
+
+    @DynamoDBHashKey(attributeName = "id")
+    private String id;
+    @DynamoDBAttribute(attributeName = "favoriteRecipes")
+    private String favoriteRecipes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFavoriteRecipes() {
+        return favoriteRecipes;
+    }
+
+    public void setFavoriteRecipes(String favoriteRecipes) {
+        this.favoriteRecipes = favoriteRecipes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRecord that = (UserRecord) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
