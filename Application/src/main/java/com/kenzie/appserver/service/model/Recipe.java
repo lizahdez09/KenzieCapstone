4e50fe64078690e724b2af46c1a33a94676fa843
epentz88 <epentz88@gmail.com>
2023-06-11 11:25:09 -0500

    User Data

--- Application/src/main/java/com/kenzie/appserver/service/model/Recipe.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/Recipe.java
@@ -1,47 +1,4 @@
 package com.kenzie.appserver.service.model;
 
 public class Recipe {
-    private String id;
-    private String name;
-    private String ingredients;
-    private String timeToPrepare;
-
-    public Recipe(String id, String name, String ingredients, String timeToPrepare){
-        this.id = id;
-        this.name = name;
-        this.ingredients = ingredients;
-        this.timeToPrepare = timeToPrepare;
-    }
-
-    public String getId(){
-        return this.id;
-    }
-
-    public void setId(String id){
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public String getTimeToPrepare() {
-        return timeToPrepare;
-    }
-
-    public void setTimeToPrepare(String timeToPrepare) {
-        this.timeToPrepare = timeToPrepare;
-    }
 }
