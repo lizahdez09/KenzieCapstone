a984c1dc5c2d3cb97f4cbcf99a5a681903182166
stephensaulen <110068857+StephenSaulen@users.noreply.github.com>
2023-06-11 20:33:41 -0500

    Adding unit Test to RecipeServiceTest

--- Application/src/test/java/com/kenzie/appserver/service/RecipeServiceTest.java
+++ Application/src/test/java/com/kenzie/appserver/service/RecipeServiceTest.java
@@ -1,6 +1,7 @@
 package com.kenzie.appserver.service;
 
 import com.kenzie.appserver.controller.model.RecipeCreateRequest;
+import com.kenzie.appserver.exceptions.RecipeNotFoundException;
 import com.kenzie.appserver.repositories.RecipeRepository;
 import com.kenzie.appserver.repositories.model.RecipeRecord;
 import com.kenzie.appserver.service.model.Recipe;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static java.util.UUID.randomUUID;
@@ -26,11 +29,46 @@ public class RecipeServiceTest {
         recipeService = new RecipeService(recipeRepository);
     }
     /** ------------------------------------------------------------------------
+     *  recipeService.getAllRecipes
+     *  ------------------------------------------------------------------------ **/
+    @Test
+    void getAllRecipes_returnsThreeRecipesInList() {
+        //GIVEN
+        String id1 = randomUUID().toString();
+        String id2 = randomUUID().toString();
+        String id3 = randomUUID().toString();
+
+        RecipeRecord record1 = createRecipeRecordWithId(id1);
+        RecipeRecord record2 = createRecipeRecordWithId(id2);
+        RecipeRecord record3 = createRecipeRecordWithId(id3);
+
+        List<RecipeRecord> recipeRecordList = new ArrayList<>();
+        recipeRecordList.add(record1);
+        recipeRecordList.add(record2);
+        recipeRecordList.add(record3);
+
+        List<Recipe> expected = new ArrayList<>();
+        expected.add(new Recipe(record1));
+        expected.add(new Recipe(record2));
+        expected.add(new Recipe(record3));
+
+        //WHEN
+        when(recipeRepository.findAll()).thenReturn(recipeRecordList);
+        List<Recipe> actual = recipeService.getAllRecipes();
+
+        //THEN
+        Assertions.assertNotNull(actual, "List is not null");
+        Assertions.assertEquals(expected.size(), actual.size(), "List size is the same");
+        Assertions.assertEquals(expected.get(0).getId(), actual.get(0).getId(), "First recipe id matches");
+        Assertions.assertEquals(expected.get(1).getId(), actual.get(1).getId(), "Second recipe id matches");
+        Assertions.assertEquals(expected.get(2).getId(), actual.get(2).getId(), "Third recipe id matches");
+    }
+
+    /** ------------------------------------------------------------------------
      *  recipeService.getRecipeById
      *  ------------------------------------------------------------------------ **/
-
     @Test
-    void getRecipeById_ReturnsRecipe() {
+    void getRecipeById_returnsRecipe() {
         //GIVEN
         String id = randomUUID().toString();
         RecipeRecord record = createRecipeRecordWithId(id);
@@ -46,31 +84,45 @@ public class RecipeServiceTest {
     }
 
     @Test
-    void getRecipeById_Returns() {
+    void getRecipeById_throwsRecipeNotFoundException() {
         //GIVEN
         String id = randomUUID().toString();
-        RecipeRecord record = createRecipeRecordWithId(id);
 
         //WHEN
-        when(recipeRepository.findById(id)).thenReturn(Optional.of(record));
-        Recipe recipe = recipeService.getRecipeById(id);
+        when(recipeRepository.findById(id)).thenReturn(Optional.empty());
 
         //THEN
-        assertNotNull(recipe, "The Recipe is returned");
-        Assertions.assertEquals(record.getId(), recipe.getId(), "The id's match");
-        Assertions.assertEquals(record.getName(), recipe.getName(), "The names match");
+        Assertions.assertThrows(RecipeNotFoundException.class, () -> recipeService.getRecipeById(id));
     }
+
     /** ------------------------------------------------------------------------
-     *  recipeService.addRecipe
+     *  recipeService.addNewRecipe
      *  ------------------------------------------------------------------------ **/
     @Test
-    public void addedRecipe_isNotNull() {
+    public void addNewRecipe_returnsRecipe() {
+        //GIVEN
+        String id = randomUUID().toString();
+        RecipeRecord record = createRecipeRecordWithId(id);
+
         RecipeCreateRequest request = new RecipeCreateRequest();
-        RecipeService recipeService = new RecipeService(recipeRepository);
-        Recipe result = recipeService.addNewRecipe(request);
-        assertNotNull(result, "The added recipe should not be null");
+        request.setName("Record");
+        request.setIngredients("Ingredients");
+        request.setTimeToPrepare("30");
+
+        //WHEN
+        when(recipeRepository.save(record)).thenReturn(record);
+        Recipe recipe = recipeService.addNewRecipe(request);
+
+        //THEN
+        assertNotNull(recipe, "The Recipe is returned");
+        Assertions.assertEquals(record.getName(), recipe.getName(), "The names match");
     }
 
+    /**
+     * Creates a RecipeRecord with a given String id
+     * @param id {@link String}
+     * @return record - {@link RecipeRecord}
+     */
     private RecipeRecord createRecipeRecordWithId(String id) {
         RecipeRecord record = new RecipeRecord();
         record.setRecipeId(id);
