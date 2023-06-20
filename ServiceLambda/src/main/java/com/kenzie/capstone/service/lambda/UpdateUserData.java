e6b8041e4482d9e82a341b3326d18cc9d882fdf0
epentz88 <epentz88@gmail.com>
2023-06-20 18:28:54 -0500

    UpdateUser and recipeClient

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/lambda/UpdateUserData.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/lambda/UpdateUserData.java
@@ -1,4 +1,53 @@
 package com.kenzie.capstone.service.lambda;
 
-public class UpdateUserData {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.kenzie.capstone.service.UserService;
+import com.kenzie.capstone.service.dependency.DaggerServiceComponent;
+import com.kenzie.capstone.service.dependency.ServiceComponent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UpdateUserData implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    static final Logger log = LogManager.getLogger();
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        log.info(gson.toJson(input));
+
+        ServiceComponent serviceComponent = DaggerServiceComponent.create();
+        UserService userLambService = serviceComponent.provideLambdaService();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+        String id = input.getPathParameters().get("id");
+        String recipes = input.getQueryStringParameters().get("favoriteRecipes");
+        String name = input.getQueryStringParameters().get("name");
+
+        try {
+            // Update user data
+            userLambService.updateUserData(id, recipes, name);
+
+            response.setStatusCode(200);
+            response.setBody(gson.toJson("Update successful"));
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setBody(gson.toJson("Update failed: " + e.getMessage()));
+        }
+
+        return response;
+    }
 }
