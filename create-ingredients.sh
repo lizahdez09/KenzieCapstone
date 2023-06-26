87c0b2dcda9c1f2e23815a28daefd201c9542b7a
bremartin95 <110068672+bremartin95@users.noreply.github.com>
2023-06-26 18:32:03 -0500

    added ingredients to ingredient script

--- create-ingredients.sh
+++ create-ingredients.sh
@@ -3,7 +3,12 @@
 API_URL="http://localhost:8080/ingredient"
 
 # Set the list of ingredients to add
-INGREDIENTS=("Milk" "Onion" "Garlic")
+INGREDIENTS=("Milk" "Onion" "Garlic" "Chicken" "Tomato" "Lettuce" "Carrot" "Spinach" "Onion" "Potato" "Cucumber" "Celery" "Cabbage"
+"Leek" "Asparagus" "Peas" "Cauliflower" "Avocado" "Pepper" "Broccoli" "Artichoke" "Pumpkin" "Radish" "Mushroom" "Corn"
+ "Grapes" "Watermelon" "Orange" "Pear" "Plum" "Pear" "Strawberry" "Blueberry" "Mango" "Papaya" "Apricot" "Banana" "Grapefruit"
+ "Lemon" "Lime" "Pineapple" "Melon" "Coconut" "Peach" "Kiwi" "Cherry" "Bread" "Rice" "Quinoa" "Flour" "Barley" "Chickpeas"
+ "Buckwheat" "Millet" "Wheat" "Oats" "Rye" "Couscous" "Hominy" "Cornmeal" "Semolina" "Bran" "Muesli" "Panko" "Matzo" "Corn Starch"
+ "Beef" "Pork" "Veal" "Shrimp" "Crab" "Turkey" "Tofu" "Soybeans" "Egg" "Yogurt" )
 
 for INGREDIENT_NAME in "${INGREDIENTS[@]}"; do
   JSON_PAYLOAD="{\"name\":\"$INGREDIENT_NAME\"}"
