93a68176805965f7916f7287556ec131f5a5ef5f
stephensaulen <110068857+StephenSaulen@users.noreply.github.com>
2023-07-07 20:29:12 -0500

    Corrections made to webpack

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -12,7 +12,6 @@ module.exports = {
     recipePage: path.resolve(__dirname, 'src', 'pages', 'recipePage.js'),
     userPage: path.resolve(__dirname, 'src', 'pages', 'userPage.js'),
     aboutPage: path.resolve(__dirname, 'src', 'pages', 'aboutPage.js'),
-    recipeBuilder: path.resolve(__dirname, 'src', 'pages', 'recipeBuilder.js'),
   },
 
   output: {
@@ -46,11 +45,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/about.html',
       filename: 'about.html',
-      inject: false
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/recipeBuilder.html',
-      filename: 'recipeBuilder.html',
       inject: false
     }),
     new HtmlWebpackPlugin({
